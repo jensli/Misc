1a389201779fd41b38166f07761252e6d4173534
Jens Li <jens@jens-box.(none)>
2012-12-06 12:30:45 +0100

    Comments

--- java/src/test/context_obj/TestContextObj.java
+++ java/src/test/context_obj/TestContextObj.java
@@ -1,16 +1,33 @@
 package test.context_obj;
+/*
 
+The Context IoC pattern is nice in that the client makes it explicit which dependencies it uses. It's not so nice because of two reasons:
 
-interface IRes1 {
+x Each client needs its own context interface.
+x Also, the global context class needs to implement the interfaces of all client classes that need the dependencies. This creates a dependency from the context class to the clients.
+
+The classes in the link below is an attempt to solve those problems.
+
+Here each publicly available service declares a "Getter" interface, which is implemented by the context class.
+
+The clients use a constructor with a type parameter constrained to types that implements the getters of the services it depends on.
+
+So the two problems mentioned above is resolved:
+    x There is an extra interface per service instead of per client.
+    x The context class depend on the getters, not on the clients context interfaces.
+
+*/
+
+interface IService1 {
     int getInt();
 
     interface Getter {
-        IRes1 getRes1();
+        IService1 getService1();
     }
 }
 
 
-class Res1 implements IRes1 {
+class Service1 implements IService1 {
     public int getInt() {
         return 3;
     }
@@ -19,16 +36,16 @@ class Res1 implements IRes1 {
 
 ////////////////////////////////////////////////////
 
-interface IRes2 {
+interface IService2 {
     String getString();
 
     interface Getter {
-        IRes2 getRes2();
+        IService2 getService2(int dummyParam);
     }
 }
 
 
-class Res2 implements IRes2 {
+class Service2 implements IService2 {
     public String getString() {
         return "Privjet mir!";
     }
@@ -36,39 +53,38 @@ class Res2 implements IRes2 {
 
 ////////////////////////////////////////////////////
 
-interface IContext extends IRes1.Getter,
-                           IRes2.Getter
+interface IContext extends IService1.Getter,
+                           IService2.Getter
 {}
 
 class Context implements IContext {
 
     @Override
-    public IRes1 getRes1() {
-        return new Res1();
+    public IService1 getService1() {
+        return new Service1();
     }
 
     @Override
-    public IRes2 getRes2() {
-        return new Res2();
+    public IService2 getService2(int dummyParam) {
+        return new Service2();
     }
-
 }
 
 ////////////////////////////////////////////////////
 
 class ClientObj {
 
-    private IRes1 res1;
-    private IRes2 res2;
+    private IService1 service1;
+    private IService2 service2;
 
     public
         <T_CONTEXT extends
-            IRes1.Getter
-          & IRes2.Getter>
+            IService1.Getter
+          & IService2.Getter>
 
     ClientObj(T_CONTEXT context) {
-        res1 = context.getRes1();
-        res2 = context.getRes2();
+        service1 = context.getService1();
+        service2 = context.getService2(2);
     }
 }
 
